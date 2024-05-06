96feb092ba8a4ac3059f94de9e7309cbcc2db121
yvinod29 <129191462+yvinod29@users.noreply.github.com>
2024-05-06 06:05:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ require('./app_api/models/db')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var tasksRouter = require('./routes/task');
+// var tasksRouter = require('./routes/task');
 
 var app = express();
 
@@ -30,7 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/task', tasksRouter);
+// app.use('/task', tasksRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
